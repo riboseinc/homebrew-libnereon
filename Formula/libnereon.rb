7093f49498ca07ff7011ba3abbc0f6650b33b28e
Jin JinRu <jin.jinru840430@gmail.com>
2018-06-15 17:12:55 +0800

    Added cmake build commands

--- Formula/libnereon.rb
+++ Formula/libnereon.rb
@@ -1,26 +1,26 @@
 class Libnereon < Formula
   desc "libnereon: Multi-Configuration Parser Library"
   homepage "https://github.com/riboseinc/libnereon"
-  url "https://github.com/riboseinc/libnereon/archive/v0.9.0.8.tar.gz"
-  sha256 "096e3c0923d16f1ba4474e9ade7fe8a914d0ed3e32179d07ea489ee9696db08d"
+  url "https://github.com/riboseinc/libnereon/archive/v0.9.1.0.tar.gz"
+  sha256 "41a758c13f2831060d8b83b66674f38011a528f5a0b4c1d9275ca78b2a312947"
   head "https://github.com/riboseinc/libnereon.git"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
+  depends_on "cmake" => :build
   depends_on "pkg-config" => :build
-  depends_on "libucl"
 
   devel do
-    version '0.9.0.8'
+    version '0.9.1.0'
   end
 
   def install
-    system './build_macos.sh'
+    system "mkdir build; cd build; cmake ..; make -j1"
     system 'make', 'install'
   end
 
   test do
-    system './tests/ucl_test'
+    system 'make test'
   end
 end
