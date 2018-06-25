9dbec47354cd80d3938f1391c6edd26e1cc98ab6
Jin JinRu <jin.jinru840430@gmail.com>
2018-06-25 15:47:14 +0800

    updated to version 0.9.1.4

--- Formula/libnereon.rb
+++ Formula/libnereon.rb
@@ -1,8 +1,8 @@
 class Libnereon < Formula
   desc "libnereon: Multi-Configuration Parser Library"
   homepage "https://github.com/riboseinc/libnereon"
-  url "https://github.com/riboseinc/libnereon/archive/v0.9.1.3.tar.gz"
-  sha256 "c958a69a48f510d4fabe471cfc00c49ed1e83eaa243cda8d969e1c74c698204c"
+  url "https://github.com/riboseinc/libnereon/archive/v0.9.1.4.tar.gz"
+  sha256 "0cdf4d894b7b7013af5a5a35efb67899d7d6d061160d89c173bf9f9c5b8eb9fd"
   head "https://github.com/riboseinc/libnereon.git"
 
   depends_on "autoconf" => :build
@@ -12,7 +12,7 @@ class Libnereon < Formula
   depends_on "pkg-config" => :build
 
   devel do
-    version '0.9.1.3'
+    version '0.9.1.4'
   end
 
   def install
